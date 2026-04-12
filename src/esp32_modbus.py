c8fedc24515d1b677f873f10921465749995754b
nexcoyocoatl <jukemaia@gmail.com>
2026-04-12 15:44:52 -0300

    Conecta e desconecta no device, pequenos ajustes, comentários

--- src/esp32_modbus.py
+++ src/esp32_modbus.py
@@ -2,33 +2,34 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtWidgets, QtCore
 from PyQt5 import uic
+import serial.tools.list_ports
 import serial
 import time
 
-import sys
-import glob
-
+DEBUG = True
 
 def start_com():
-    # devices = find_serial_ports()
-
-    # ser = serial.Serial(devices[0], 9600, timeout=1)
-
     window.pushbutton_start_com.setEnabled(False)
     window.progressbar_serial_com.setValue(30)
 
-    # Inserir conexão à porta
+    # Abre porta caso ainda não esteja aberta
+    if not ser.isOpen():
+        ser.open()
 
     window.progressbar_serial_com.setValue(60)
 
-    data = ser.readline().decode().strip() # Clear buffer? só dar read?
-    print(data) # debug
+    # Limpa o buffer
+    while ser.read():
+        pass
 
     # Inserir algo a mais?
 
     window.progressbar_serial_com.setValue(100)
-    print("start")
 
+    if DEBUG:
+        print("Application started")
+
+    # Habilita todos outros botões
     window.pushbutton_stop_com.setEnabled(True)
     window.pushbutton_read_temp.setEnabled(True)
     window.pushbutton_read_humid.setEnabled(True)
@@ -40,11 +41,21 @@ def stop_com():
 
     window.progressbar_serial_com.setValue(60)
 
-    ser = None
+    ser.close()
+
+    window.progressbar_serial_com.setValue(30)
+
+    # Caso necessário esperar parar de ler
+    if ser.isOpen():
+        while ser.read():
+            pass
 
     window.progressbar_serial_com.setValue(0)
-    print("stop")
+    
+    if DEBUG:
+        print("Application stopped")
 
+    # Desabilita todos botões com excessão do início de conexão
     window.pushbutton_start_com.setEnabled(True)
     window.pushbutton_read_temp.setEnabled(False)
     window.pushbutton_read_humid.setEnabled(False)
@@ -54,51 +65,49 @@ def stop_com():
 def read_temp():
     ser.write(b'T\n')
     data = ser.readline().decode().strip()
-    print(data) # debug
+
+    if DEBUG:
+        print("Data received: " + data)
+    
     temp = float(data.split(":")[1])
     window.lcd_temp1.display(temp)
 
 def read_humid():
     ser.write(b'H\n')
     data = ser.readline().decode().strip()
-    print(data) # debug
+    
+    if DEBUG:
+        print("Data received: " + data)
+
     hum = int(data.split(":")[1])
     window.progressbar_humid1.setValue(hum)
 
 def read_temp_humid():
     ser.write(b'th\n')
     data = ser.readline().decode().strip()
-    print(data) # debug
+    
+    if DEBUG:
+        print("Data received: " + data)
+
     t, h = data.split(":")[1].split(",")
     window.lcd_temp2.display(float(t))
     window.progressbar_humid2.setValue(int(h))
 
 def custom_command():
+    # TODO: Inserir
     print(f"custom command: {window.lineedit_custom_command.text()}")
 
-def find_serial_ports():    
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-
 if __name__ == '__main__':
-    device = '/dev/ttyUSB0' # TROCAR O SERIAL PELO QUE A ESP TÀ SENDO MONTADA
+    # Descobre portas que contenham UART na descrição    
+    for port in serial.tools.list_ports.comports():
+        # print(f"{port.device}: {port.description}")
+        if ("UART" in port.description):
+            device = port.device
+
+            if DEBUG:
+                print("Device " + port.device + " found")
+            break
+
     ser = serial.Serial(device, 9600, timeout=1)
 
     app = QtWidgets.QApplication([])
